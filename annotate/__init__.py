cf274caa66f26eee1332b9fd07cbde4dbfe1ce2f
noahbenson <nben@uw.edu>
2022-11-01 15:27:53 -0700

    first commit

---
+++ annotate/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+################################################################################
+# annotate/__init__.py
+#
+# Initialization code for the annotate toolkit.
+# The annotate toolkit primarily uses the neuropythy library
+# (github.com/noahbenson/neuropythy) to facilitate the annotation of cortical
+# surfaces by hand using a combination of Jupyter notebooks, Git/GitHub, and
+# (optionally) Docker. 
+
+'''
+The annotate package facilitates the manual annotation of the human cortex.
+
+These tools are intended to be run using a Docker container; for information
+on how to use these tools, see the README.md file in the github repository
+noahbenson/cortex-annotate.
+'''
+
+def DrawingTool():
+    print("The drawing tool is not yet ready.")
