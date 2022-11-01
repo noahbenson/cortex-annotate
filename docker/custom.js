c5f9e5fbbf361e83ed060a23c2ce151e1ea6f3fb
noahbenson <nben@uw.edu>
2022-11-01 15:25:01 -0700

    added code to autorun cells and fixed the hide-toolbar code

--- docker/custom.js
+++ docker/custom.js
@@ -12,10 +12,13 @@ define(['base/js/events'], function(events) {
 */
 
 Jupyter.notebook.events.one('kernel_ready.Kernel', () => {
-    console.log('custom.js: hiding header and toolbar');
+    console.log('custom.js: Hiding header and toolbar...');
     $('#header-container').toggle();
     $('.header-bar').toggle();
     $('div#maintoolbar').toggle();
-    events.trigger('resize-header.Page');
+    Jupyter.notebook.events.trigger('resize-header.Page');
+    // Auto-run cells and hide input cells.
+    console.log("custon.js: Auto-running all cells...");
+    Jupyter.actions.call("jupyter-notebook:run-all-cells");
+    $('div.input').hide();
 });
-
